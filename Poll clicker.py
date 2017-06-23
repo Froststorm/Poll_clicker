ac22408382df6b922e9cc120e6b23e9089c87843
Andrey <andrey.paladin@gmail.com>
2017-06-23 18:01:29 +0300

    Сделал цикл на 5 посторений, Иногда браузер(Firefox) не хочет останавливать страницу, ещe не разобрался почему.

--- Poll clicker.py
+++ Poll clicker.py
@@ -1,31 +1,53 @@
 # -*- coding: utf-8 -*-
+
 import codecs
 import random
+
 import list_of_locators as lst
 from selenium import webdriver
-
-driver = webdriver.Firefox()
-driver.implicitly_wait(5)
-base_url = "https://www.survio.com/"
-
-try:
-    # driver = webdriver.Firefox()
-    driver.set_page_load_timeout(4)
-    driver.get(base_url + "/survey/d/O7F2Y9I0V9J0Y7J4S")
-except Exception:
-    print('time out')
-    driver.execute_script("window.stop();")
-
-for i in lst.listing:
-    driver.find_element_by_xpath(random.choice(i)).click()
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-driver.find_element_by_xpath("//textarea").clear()
-driver.find_element_by_xpath("//textarea").send_keys(u"Купить Ире Оксюковской новый стул")
-driver.find_element_by_xpath("//fieldset[9]/div/label/textarea").clear()
+def clicker_method():
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.cache.disk.enable", False)
+    profile.set_preference("browser.cache.memory.enable", False)
+    profile.set_preference("browser.cache.offline.enable", False)
+    profile.set_preference("network.http.use-cache", False)
+    driver = webdriver.Firefox(profile)
+    driver.implicitly_wait(4)
+    base_url = "https://www.survio.com/"
 
-# чтение не юникодовского файла (кирилица)
-randname = random.choice(list(codecs.open('names.txt', encoding='1251').readlines()))
-driver.find_element_by_xpath("//fieldset[9]/div/label/textarea").send_keys(randname)
+    try:
+        driver.get(base_url + "/survey/d/O7F2Y9I0V9J0Y7J4S")
+        # wait = WebDriverWait(driver, 5)
+        # element = wait.until(EC.title_contains('Удовлетворенность'))
+        driver.set_page_load_timeout(5)
+        driver.execute_script("window.stop();")
 
-driver.refresh()
+    except Exception:
+        print('time out')
+        driver.execute_script("window.stop();")
+    # заполняется основная часть опроса
+    for i in lst.listing:
+        driver.find_element_by_xpath(random.choice(i)).click()
+    # заполняются текстовые поля
+    driver.find_element_by_xpath("//textarea").clear()
+    driver.find_element_by_xpath("//textarea").send_keys(u"Купить Ире Оксюковской новый стул")
+    driver.find_element_by_xpath("//fieldset[9]/div/label/textarea").clear()
+
+    # чтение не юникодовского файла (кирилица)
+    randname = random.choice(list(codecs.open('names.txt', encoding='1251').readlines()))
+    driver.find_element_by_xpath("//fieldset[9]/div/label/textarea").send_keys(randname)
+    driver.delete_all_cookies()
+
+    driver.close()
+
+
+with open('names.txt', 'r') as file:
+    for i in range(5):
+        print(i+1)
+        clicker_method()
+
+    webdriver.Firefox.quit()
--- readnames.py
+++ readnames.py
@@ -1,8 +1,12 @@
 # -*- coding: utf8 -*-
-import random
-import codecs
+# import random
+# import codecs
 
 
-randname = random.choice(list(codecs.open('c:\\names.txt', encoding='1251').readlines()))
 
-print(randname)
+# randname = random.choice(list(codecs.open('c:\\names.txt', encoding='1251').readlines()))
+
+randname = open('names.txt', 'r')
+
+for i in randname.readlines():
+    print(i)
