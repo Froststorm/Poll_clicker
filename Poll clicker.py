8794a117ae58762240d8931480462c6f6ab54506
Andrii <andrey.paladin@gmail.com>
2017-06-25 17:24:50 +0300

    Кое что поменял

--- Poll clicker.py
+++ Poll clicker.py
@@ -1,53 +1,60 @@
 # -*- coding: utf-8 -*-
 
-import codecs
+# import codecs
 import random
+from time import sleep
+import time
 
 import list_of_locators as lst
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC, wait
+
+driver = webdriver.Chrome()
+driver.set_window_size(800, 600)
+driver.set_page_load_timeout(5)
+base_url = 'https://www.survio.com/survey/d/O7F2Y9I0V9J0Y7J4S'
 
 
 def clicker_method():
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("browser.cache.disk.enable", False)
-    profile.set_preference("browser.cache.memory.enable", False)
-    profile.set_preference("browser.cache.offline.enable", False)
-    profile.set_preference("network.http.use-cache", False)
-    driver = webdriver.Firefox(profile)
-    driver.implicitly_wait(4)
-    base_url = "https://www.survio.com/"
-
-    try:
-        driver.get(base_url + "/survey/d/O7F2Y9I0V9J0Y7J4S")
-        # wait = WebDriverWait(driver, 5)
-        # element = wait.until(EC.title_contains('Удовлетворенность'))
-        driver.set_page_load_timeout(5)
-        driver.execute_script("window.stop();")
-
-    except Exception:
-        print('time out')
-        driver.execute_script("window.stop();")
-    # заполняется основная часть опроса
-    for i in lst.listing:
-        driver.find_element_by_xpath(random.choice(i)).click()
-    # заполняются текстовые поля
-    driver.find_element_by_xpath("//textarea").clear()
-    driver.find_element_by_xpath("//textarea").send_keys(u"Купить Ире Оксюковской новый стул")
-    driver.find_element_by_xpath("//fieldset[9]/div/label/textarea").clear()
-
-    # чтение не юникодовского файла (кирилица)
-    randname = random.choice(list(codecs.open('names.txt', encoding='1251').readlines()))
-    driver.find_element_by_xpath("//fieldset[9]/div/label/textarea").send_keys(randname)
-    driver.delete_all_cookies()
-
-    driver.close()
+	# first_result = WebDriverWait(driver, 5).until(lambda browser: browser.find_element_by_class_name('input-group-addon'))
+	# first_link = first_result.find_element_by_tag_name('a')
+	driver.get(base_url)
+	starttime = time.time()
+	print(starttime)
+	# driver.execute_script('window.stop();')
+	sleep(1)
+	stoptime = time.time()
+	print(starttime - stoptime)
+	# WebDriverWait.until(EC.title_contains('Удовлетворенность'))
 
 
-with open('names.txt', 'r') as file:
-    for i in range(5):
-        print(i+1)
-        clicker_method()
+	# print('time out')
+	# driver.execute_script('window.stop();')
+	# заполняется основная часть опроса
 
-    webdriver.Firefox.quit()
+	for locator in lst.listing:
+		# print(locator)
+		choice = random.choice(locator)
+		print(choice)
+		driver.find_element_by_xpath(choice).click()
+
+	# заполняются текстовые поля
+	# driver.find_element_by_xpath('//fieldset[8]/div/label/textarea').click()
+	# driver.find_element_by_xpath('//fieldset[8]/div/label/textarea').send_keys('write something')
+
+	driver.find_element_by_xpath('//fieldset[9]/div/label/textarea').clear()
+
+	# рандомное чтение  строк из файла
+	randname = random.choice(open('names.txt').read().splitlines())
+	driver.find_element_by_xpath('//fieldset[9]/div/label/textarea').send_keys(randname)
+	driver.delete_all_cookies()
+	driver.close()
+
+
+# with open('names.txt', 'r') as file:
+for i in range(5):
+	print(i + 1)
+	clicker_method()
+
+webdriver.Firefox.quit()
--- list_print.py
+++ list_print.py
@@ -1,4 +1,12 @@
-import list_of_locators as lst
+import codecs
 
-for i in lst.listing:
-    print(i)
+import list_of_locators as lst
+import random
+
+myline =random.choice(open('names.txt').read().splitlines())
+print(myline)
+
+
+# with open('names.txt') as f:
+# 	for name in f:
+# 		print(name)
